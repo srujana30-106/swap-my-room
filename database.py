673428b4c7cf2380f9c67ee8cfdfc12452ecbaff
srujana30-106 <srujanasanaka970@gmail.com>
2025-07-08 16:11:25 +0530

    Fixed merge conflicts and updated database models

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from app import app
 from models import db, User, RoomPreference, SwapRequest  # make sure models are imported
 
 if __name__ == '__main__':
-    with app.app_context():
-        
+    with app.app_context():      
         db.create_all()
         print("Done.")
