3805ce9028cd850aa06b232c766b32a74a094699
srujana30-106 <srujanasanaka970@gmail.com>
2025-07-08 16:59:35 +0530

    Fixed merge conflicts and updated database models

--- app.py
+++ app.py
@@ -342,4 +342,4 @@ def handle_request_sent(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
