dad4c47c302eadd2e128112a6d8b7d2e65e44700
srujana30-106 <srujanasanaka970@gmail.com>
2025-07-08 15:25:12 +0530

    Fixed merge conflicts and updated database models

--- database.py
+++ database.py
@@ -3,9 +3,6 @@ from models import db, User, RoomPreference, SwapRequest  # make sure models are
 
 if __name__ == '__main__':
     with app.app_context():
-        print("Dropping all tables...")
-        db.reflect()
-        db.drop_all()
-        print("Creating all tables...")
+        
         db.create_all()
         print("Done.")
